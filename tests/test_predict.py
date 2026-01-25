d164dc3ae13c6358772d04b0e14ca6688ae8ca3e
katiravankrishnanai-byte <katiravankrishnanai@gmail.com>
2026-01-26 02:32:15 +0800

    Update test_predict.py

--- tests/test_predict.py
+++ tests/test_predict.py
@@ -2,22 +2,19 @@ from fastapi.testclient import TestClient
 from src.app import app
 
 def test_predict_endpoint():
-    client = TestClient(app)
-
-    payload = {
-        "Acceleration": 5.0,
-        "TopSpeed_KmH": 180,
-        "Range_Km": 420,
-        "Battery_kWh": 75,
-        "Efficiency_WhKm": 170,
-        "FastCharge_kW": 150,
-        "Seats": 5,
-        "PriceEuro": 45000,
-        "PowerTrain": "AWD",
-    }
-
-    res = client.post("/predict", json=payload)
-    assert res.status_code == 200
-    body = res.json()
-    assert "prediction" in body
-    assert isinstance(body["prediction"], (int, float))
+    with TestClient(app) as client:
+        payload = {
+            "Acceleration": 5.0,
+            "TopSpeed_KmH": 180,
+            "Range_Km": 420,
+            "Battery_kWh": 75,
+            "Efficiency_WhKm": 170,
+            "FastCharge_kW": 150,
+            "Seats": 5,
+            "PriceEuro": 45000,
+            "PowerTrain": "AWD",
+        }
+        res = client.post("/predict", json=payload)
+        assert res.status_code == 200
+        body = res.json()
+        assert "prediction" in body
